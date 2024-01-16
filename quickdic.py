79e40c9a998c187439a453ea465d74b572e34ebb
Passarb <johndoe@example.com>
2024-01-16 10:21:37 -0500

    u

--- quickdic.py
+++ quickdic.py
@@ -1,8 +1,8 @@
+from pwnagotchi import plugins
 import logging
 import subprocess
 import string
 import re
-import pwnagotchi.plugins as plugins
 
 '''
 Aircrack-ng needed, to install:
@@ -14,15 +14,23 @@ Cracked handshakes stored in handshake folder as [essid].pcap.cracked
 
 class QuickDic(plugins.Plugin):
     __author__ = 'pwnagotchi [at] rossmarks [dot] uk'
-    __version__ = '1.0.0'
+    __version__ = '2.0.0'
     __license__ = 'GPL3'
     __description__ = 'Run a quick dictionary scan against captured handshakes'
+    __dependencies__ = {
+        'apt': ['aircrack-ng'],
+    }
+    __defaults__ = {
+        'enabled': False,
+        'wordlist_folder': '/opt/wordlists/',
+        'face': '(·ω·)',
+    }
 
     def __init__(self):
         self.text_to_set = ""
 
     def on_loaded(self):
-        logging.info("Quick dictionary check plugin loaded")
+        logging.info('[quickdic] plugin loaded')
 
         if 'face' not in self.options:
             self.options['face'] = '(·ω·)'
@@ -31,26 +39,27 @@ class QuickDic(plugins.Plugin):
             ('/usr/bin/dpkg -l aircrack-ng | grep aircrack-ng | awk \'{print $2, $3}\''), shell=True, stdout=subprocess.PIPE)
         check = check.stdout.decode('utf-8').strip()
         if check != "aircrack-ng <none>":
-            logging.info("quickdic: Found " + check)
+            logging.info('[quickdic] Found %s', check)
         else:
-            logging.warning("aircrack-ng is not installed!")
+            logging.warning('[quickdic] aircrack-ng is not installed!')
 
     def on_handshake(self, agent, filename, access_point, client_station):
         display = agent.view()
         result = subprocess.run(('/usr/bin/aircrack-ng ' + filename + ' | grep "1 handshake" | awk \'{print $2}\''),
                                 shell=True, stdout=subprocess.PIPE)
-        result = result.stdout.decode('utf-8').translate({ord(c): None for c in string.whitespace})
+        result = result.stdout.decode(
+            'utf-8').translate({ord(c): None for c in string.whitespace})
         if not result:
-            logging.info("[quickdic] No handshake")
+            logging.info('[quickdic] No handshake')
         else:
-            logging.info("[quickdic] Handshake confirmed")
+            logging.info('[quickdic] Handshake confirmed')
             result2 = subprocess.run(('aircrack-ng -w `echo ' + self.options[
-                'wordlist_folder'] + '*.txt | sed \'s/\ /,/g\'` -l ' + filename + '.cracked -q -b ' + result + ' ' + filename + ' | grep KEY'),
+                'wordlist_folder'] + '*.txt | sed \'s/ /,/g\'` -l ' + filename + '.cracked -q -b ' + result + ' ' + filename + ' | grep KEY'),
                 shell=True, stdout=subprocess.PIPE)
             result2 = result2.stdout.decode('utf-8').strip()
-            logging.info("[quickdic] " + result2)
+            logging.info('[quickdic] %s', result2)
             if result2 != "KEY NOT FOUND":
-                key = re.search('\[(.*)\]', result2)
+                key = re.search(r'\[(.*)\]', result2)
                 pwd = str(key.group(1))
                 self.text_to_set = "Cracked password: " + pwd
                 display.update(force=True)
